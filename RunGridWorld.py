b2a4f65dd2d3db3903d7c0550f4364253b8e5015
CharlieGB <thecharlespope@gmail.com>
2024-06-17 17:01:22 +0100

    Further changes for Birkbeck server code

--- RunGridWorld.py
+++ RunGridWorld.py
@@ -2,7 +2,9 @@ import Utilities
 from GridWorld.Functions.RandomSeedSweep import RunRandomSeedSweep
 from GridWorld.Functions.MazeTypeSweep import RunMazeTypeSweep
 from GridWorld.Functions.RevaluationSweep import RunRevaluationSweep
+from GridWorld.Functions.HumanMazeSweep import HumanMazeSweep
 
 Utilities.InitialiseGPU()
 
-RunMazeTypeSweep()
+HumanMazeSweep()
+#RunMazeTypeSweep()
